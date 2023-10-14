ba9bb1e25f32898540fb7bf9613e75d373b95513
GuyInTheShell <guyintheshell@gmail.com>
2023-10-14 10:15:36 +0200

    feat: more styling and images

--- GIT/Manim_videos/1_IntroToSCM/editor_timeline.py
+++ GIT/Manim_videos/1_IntroToSCM/editor_timeline.py
@@ -1,34 +1,57 @@
-from manim import * # type: ignore
+from pathlib import Path
+
+from manim import ORIGIN, Arrow, Create, FadeIn, ImageMobject, Line, Scene, Text, config
 from numpy import array
 
-from manim_utils import LEFT_SIDE, RIGHT_SIDE
+from manim_utils import H_UNIT, LEFT_SIDE, PALETTES, RIGHT_SIDE, V_UNIT
 
+PALETTE = PALETTES["BlackGrayYellowNightGradient"]
 
-config.background_color = "#333333"
+config.background_color = PALETTE[0]
 config.frame_height = 9
+
+TEXT_COLOR = PALETTE[3]
+
+SCENE_DIR = Path(__file__).parent.absolute()
+RESOURCES_DIR = SCENE_DIR.joinpath("resources")
 
 
 class EditorTimeline(Scene):
     def construct(self):
         time_width = config.frame_width - 2
-        time = Arrow(start=array([LEFT_SIDE + 1, 0., 0.]), end=array([RIGHT_SIDE - 1, 0., 0.]))
+        time = Arrow(start=array([LEFT_SIDE + 1, 0., 0.]), end=array([RIGHT_SIDE - 1, 0., 0.]), color=TEXT_COLOR)
         self.add(time)
 
         elems = [
-            {"text": "Office Tools"},
-            {"text": "Wiki"},
-            {"text": "Collaborative Edition"},
+            {"text": "Office Tools", "images": ["ppt.png", "xls.png", "doc.png"]},
+            {"text": "Wiki", "images": ["Wikipedia.png"]},
+            {"text": "Collaborative Edition", "images": ["CollaborativeEditing.png"]},
         ]
 
         interval = time_width / (len(elems) + 1)
         pos = - time_width / 2 + interval
         y_dir = 1
         for elem in elems:
-            event = Line(start=array([pos, 0., 0.]), end=array([pos, y_dir * 2., 0.]))
+            event = Line(start=array([pos, 0., 0.]), end=array([pos, y_dir * V_UNIT, 0.]))
             self.play(Create(event))
-            event_txt = Text(elem["text"])
-            event_txt.move_to(array([pos, y_dir * 3., 0.]))
+            event_txt = Text(elem["text"], color=TEXT_COLOR)
+            event_txt.move_to(array([pos, y_dir * 2 * V_UNIT, 0.]))
+
+            imgs = []
+            h_offset = - (len(elem["images"]) // 2) * 0.5 * H_UNIT
+            v_offset = 0
+            for img in elem["images"]:
+                image = ImageMobject(RESOURCES_DIR.joinpath(img))
+                image.height = 1 * V_UNIT
+                image.move_to(array([pos + h_offset, y_dir * 3 * V_UNIT + v_offset * 0.3 * V_UNIT, 0.]), ORIGIN)
+                imgs.append(image)
+
+                h_offset += 0.5 * H_UNIT
+                v_offset += 0.5 * V_UNIT
+
             self.play(Create(event_txt))
+            for image in imgs:
+                self.play(FadeIn(image))
 
             self.wait()
 
--- GIT/Manim_videos/manim_utils.py
+++ GIT/Manim_videos/manim_utils.py
@@ -1,5 +1,15 @@
 from manim import config
 
+PALETTES = {
+    # https://colorhunt.co/palette/27282961677ad8d9dafff6e0
+    "BlackGrayYellowNightGradient": ["#272829", "#61677A", "#D8D9DA", "#FFF6E0"],
+    # https://colorhunt.co/palette/2c3333395b64a5c9cae7f6f2
+    "DarkBlackNavyTealColdNightGradientSea": ["#2C3333", "#395B64", "#A5C9CA", "#E7F6F2"],
+}
+
+H_UNIT = config.frame_width / 16 / 2
+V_UNIT = config.frame_width / 9 / 2
+
 TOP = 0
 BOTTOM = 0
 LEFT_SIDE = 0
